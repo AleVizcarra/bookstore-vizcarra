aa2e4c0f211d5575d55244ef3b74764877e98920
Ale Vizcarra <alevizcarrapena@gmail.com>
2022-05-31 16:51:16 -0600

    Modificacion validaciones tel input

--- src/components/checkout/FormularioCompra.js
+++ src/components/checkout/FormularioCompra.js
@@ -129,21 +129,24 @@ const FormularioCompra = () => {
                 </svg>
             </div>
 
-            <div className='input-container'>
-                <input 
-                    type="tel"
-                    placeholder='Teléfono (solo números): 6699471234' 
-                    name="phone"
-                    value={phone}
-                    onChange = {handleTelInput}
-                    pattern="[0-9]{10}"
-                    minLength={10}
-                    maxLength={10}
-                    required 
-                />
-                <svg className='input-icon' width="24" height="24" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M18.1182 14.702L14 15.5C11.2183 14.1038 9.5 12.5 8.5 10L9.26995 5.8699L7.81452 2L4.0636 2C2.93605 2 2.04814 2.93178 2.21654 4.04668C2.63695 6.83 3.87653 11.8765 7.5 15.5C11.3052 19.3052 16.7857 20.9564 19.802 21.6127C20.9668 21.8662 22 20.9575 22 19.7655L22 16.1812L18.1182 14.702Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
+            <div>
+                <div className="input-container">
+                    <input
+                        type="tel"
+                        placeholder='Teléfono: 1234567890'
+                        name="phone"
+                        value={phone}
+                        onChange = {handleTelInput}
+                        pattern="[0-9]{10}"
+                        minLength={10}
+                        maxLength={10}
+                        required
+                    />
+                    <svg className='input-icon' width="24" height="24" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M18.1182 14.702L14 15.5C11.2183 14.1038 9.5 12.5 8.5 10L9.26995 5.8699L7.81452 2L4.0636 2C2.93605 2 2.04814 2.93178 2.21654 4.04668C2.63695 6.83 3.87653 11.8765 7.5 15.5C11.3052 19.3052 16.7857 20.9564 19.802 21.6127C20.9668 21.8662 22 20.9575 22 19.7655L22 16.1812L18.1182 14.702Z" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"/>
+                    </svg>
+                </div>
+                <p className='tel-pattern'>10 dígitos (solo números)</p>
             </div>
 
             <div className='input-container'>
